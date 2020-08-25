e0e551737dcf9595050db83b59147e354a0999cf
kohbanye <kohbanye@gmail.com>
2020-08-26 08:47:30 +0900

    Revert "import CompositionAPI"
    This reverts commit 755ff04dc74c724bf87d63d5998866665ad033ae.

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import router from './router'
 import store from './store'
 import App from './App.vue'
 import './index.scss'
-import VueCompositionApi from '@vue/composition-api'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
 app.mount('#app')
-app.use(VueCompositionApi)
