91dcd8e9f8373da34ded9366534966ec4c773960
kohbanye <kohbanye@gmail.com>
2020-08-28 19:22:54 +0900

    import axios

--- src/pages/Explorer.vue
+++ src/pages/Explorer.vue
@@ -14,6 +14,7 @@
 </template>
 
 <script lang="ts">
+import axios from 'axios'
 import router from '/@/router'
 import { reactive } from 'vue'
 import DropdownMenu from '/@/components/DropdownMenu.vue'
