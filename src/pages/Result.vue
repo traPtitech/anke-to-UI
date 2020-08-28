61d4ffd92eb408936cba11321eebf3ce06c7cdb3
Fogrexon <fogrexon@gmail.com>
2020-08-29 01:27:50 +0900

    ダミーデータ挿入

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -40,26 +40,168 @@
 </template>
 
 <script lang="ts">
+// import InformationSummary from '@/components/Information/InformationSummary'
+import {
+  defineComponent,
+  reactive,
+  computed,
+  UnwrapNestedRefs,
+  ComputedRef
+} from 'vue'
+import { useRouter } from 'vue-router'
+import common from '@/bin/common'
+import {
+  getQuestionnaire,
+  getMyResponses,
+  getResults,
+  getQuestions
+} from '@/lib/api'
 import Routes from '/@/components/Routes.vue'
 import Individual from '@/components/Results/Individual'
 import Statistics from '@/components/Results/Statistics'
 import Spreadsheet from '@/components/Results/Spreadsheet'
-// import InformationSummary from '@/components/Information/InformationSummary'
-import { defineComponent, reactive } from 'vue'
 
 export default defineComponent({
   name: 'Result',
   components: {
     Routes
   },
-  setup(props) {
-    const state = reactive({
-      information: {}
+  setup(props, context) {
+    const state: UnwrapNestedRefs<any> = reactive({
+      information: {},
+      hasResponded: false,
+      canViewResults: false,
+      results: [],
+      questions: [],
+      questionData: [],
+      responseData: {}
     })
 
+    const questionnaireId: ComputedRef<number> = computed(
+      () => context.root.$route.path
+    )
+    const query: ComputedRef<string> = computed(
+      () => context.root.$route.query.tab
+    )
+    // nanikashokikakansu(state, questionnaireId.value)
+    dummy(state, questionnaireId.value, query.value)
+
     return {
-      state
+      state,
+      detailTabs: ['Statistics', 'Spreadsheet', 'Individual']
     }
   }
 })
+
+const dummy = (state: any, id: number, query: string) => {
+  setTimeout(() => {
+    _dummy(state, id, query)
+  }, 0.2)
+}
+
+const _dummy = (state: any, id: number, query: string) => {
+  state.information = {
+    administrators: ['Fogrex'],
+    created_at: new Date('1990-01-01T12:00:00+09:00'),
+    description: 'テスト質問です。ダミー',
+    modified_at: new Date('1990-01-01T12:00:00+09:00'),
+    questionnaireID: 001,
+    res_shared_to: 'public',
+    res_time_limit: 'NULL',
+    respondents: ['Fogrex', 'Ogrex', 'Xergof'],
+    targets: [],
+    title: 'テスト質問'
+  }
+  state.hasResponded = true
+  state.canViewResults = false
+  state.results = [
+    {
+      modifiedAt: '1990/01/01 12:00',
+      responseId: 0001,
+      responseBody: [
+        {
+          option_response: [],
+          questionID: 0001,
+          question_type: 'TextArea',
+          response: 'これはテストですか？'
+        },
+        {
+          option_response: [],
+          questionID: 0002,
+          question_type: 'TextArea',
+          response: 'これはテストです'
+        }
+      ],
+      submittedAt: '1990/01/01 12:00',
+      traqId: 'Fogrex'
+    },
+    {
+      modifiedAt: '1990/01/01 12:00',
+      responseId: 0002,
+      responseBody: [
+        {
+          option_response: [],
+          questionID: 0001,
+          question_type: 'TextArea',
+          response: 'これはテストですか？'
+        },
+        {
+          option_response: [],
+          questionID: 0002,
+          question_type: 'TextArea',
+          response: 'これはテストです'
+        }
+      ],
+      submittedAt: '1990/01/01 12:00',
+      traqId: 'Ogrex'
+    },
+    {
+      modifiedAt: '1990/01/01 12:00',
+      responseId: 0003,
+      responseBody: [
+        {
+          option_response: [],
+          questionID: 0001,
+          question_type: 'TextArea',
+          response: 'これはテストですか？'
+        },
+        {
+          option_response: [],
+          questionID: 0002,
+          question_type: 'TextArea',
+          response: 'これはテストです'
+        }
+      ],
+      submittedAt: '1990/01/01 12:00',
+      traqId: 'Xergof'
+    }
+  ]
+  state.questions = ['質問', '言いたいこと']
+  state.questionData = [
+    {
+      questionId: 0001,
+      type: 'TextArea',
+      component: 'short-answer',
+      questionBody: '質問',
+      isRequired: true,
+      pageNum: 1,
+      responseBody: ''
+    },
+    {
+      questionId: 0002,
+      type: 'TextArea',
+      component: 'short-answer',
+      questionBody: '言いたいこと',
+      isRequired: true,
+      pageNum: 1,
+      responseBody: ''
+    }
+  ]
+  state.responseData = state.results[0]
+  let newBody: any = {}
+  state.responseData.responseBody.forEach((data: any) => {
+    newBody[data.questionID] = data
+  })
+  state.responseData.body = newBody
+}
 </script>
