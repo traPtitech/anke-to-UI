95ff956df087dac5f0ec621703673040d05abe26
Fogrexon <fogrexon@gmail.com>
2020-09-05 15:06:10 +0900

    add icon

--- src/assets/mdi.ts
+++ src/assets/mdi.ts
@@ -2,14 +2,19 @@ import {
   mdiChevronDown,
   mdiMagnify,
   mdiOpenInNew,
-  mdiHelpCircleOutline
+  mdiHelpCircleOutline,
+  mdiEyeOff,
+  mdiEye
 } from '@mdi/js'
 
 const mdi = new Map<string, string>([
   ['chevron-down', mdiChevronDown],
   ['magnify', mdiMagnify],
   ['open-in-new', mdiOpenInNew],
-  ['help-circle-outline', mdiHelpCircleOutline]
+  ['help-circle-outline', mdiHelpCircleOutline],
+  ['chevron-down', mdiChevronDown],
+  ['eye-closed', mdiEyeOff],
+  ['eye', mdiEye]
 ])
 
 export default mdi
