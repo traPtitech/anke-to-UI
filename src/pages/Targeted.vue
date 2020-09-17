918c7ba5d7a015623531a909e9834cc463a61088
annin3 <shunta.titech@gmail.com>
2020-09-17 13:08:50 +0900

    component一個省いた

--- src/pages/Targeted.vue
+++ src/pages/Targeted.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="wrapper">
-    <List />
+    <table :class="$style.table">
+      <tr :class="$style.header">
+        回答対象になっているアンケート
+      </tr>
+      <Card />
+    </table>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import Icon from '/@/components/UI/Icon.vue'
-import List from '/@/components/Targeted/List.vue'
-
+import Card from '/@/components/Targeted/Card.vue'
 export default defineComponent({
-  name: 'Targeted',
+  name: 'List',
   components: {
-    List
+    Card
   }
 })
 </script>
+
+<style lang="scss" module>
+.table {
+  width: 100%;
+  max-width: fit-content;
+  margin: 1em 0;
+  border: solid 1.5px #d9d9d9;
+  border-collapse: collapse;
+  box-shadow: 0 2px 3px #dfe0d7;
+}
+.header {
+  text-align: left;
+  font-size: 15pt;
+  margin: 8px 0;
+}
+</style>
