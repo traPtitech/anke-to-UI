712a5d31d98aab7c145cc357a86dcf7543a702d6
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-10-19 10:11:20 +0900

    add Header

---
+++ src/components/Navigation/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="$style.container">
+    <div :class="$style.title">anke-to</div>
+    <route />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Route from '/@/components/Routes.vue'
+
+export default defineComponent({
+  name: 'Header',
+  components: { Route },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style module>
+.container {
+  grid-area: header;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+}
+.title {
+  padding: 1rem;
+}
+</style>
