f224a514544bc6334ae2fb1be8c369f4238ccb98
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-10-19 09:43:52 +0900

    fix Route

--- src/components/Routes.vue
+++ src/components/Routes.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <router-link :to="{ name: 'index' }">Index</router-link>
-    <span>|</span>
-    <router-link :to="{ name: 'hello' }">Hello</router-link>
+    <router-link :to="pages[0].path">{{ pages[0].name }}</router-link>
+    <template v-for="page in pages.slice(1)" :key="page.path">
+      <span>|</span>
+      <router-link :to="page.path">
+        {{ page.name }}
+      </router-link>
+    </template>
   </div>
 </template>
 
@@ -12,12 +16,19 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'Routes',
   setup() {
-    return {}
+    const pages = [
+      { path: '/', name: 'Home' },
+      { path: '/explorer', name: 'Explorer' },
+      { path: '/responses', name: 'Responses' },
+      { path: '/targeted', name: 'Targeted' }
+    ]
+
+    return { pages }
   }
 })
 </script>
 
-<style module>
+<style lang="scss" module>
 .re {
   font-weight: bold;
 }
