8a731d59473bb9fed5f0e6912fd4aea48739bf71
kohbanye <kohbanye@gmail.com>
2021-01-09 16:44:12 +0900

    fix

--- src/components/Explorer/use/useOptions.ts
+++ src/components/Explorer/use/useOptions.ts
@@ -66,24 +66,25 @@ export const getRelativeTime = (previous: string): string => {
   const now = new Date()
   const prv = new Date(previous)
 
-  const minute = 60
-  const hour = minute * 60
-  const day = hour * 24
-  const month = day * 30.4
-  const year = day * 365
+  const MINUTE = 60
+  const HOUR = MINUTE * 60
+  const DAY = HOUR * 24
+  const MONTH = DAY * 30.4
+  const YEAR = DAY * 365
 
   const secondsDifference = (now.getTime() - prv.getTime()) / 1000
 
-  if (secondsDifference < minute) {
-    return Math.round(secondsDifference).toString() + '秒前'
-  } else if (secondsDifference < hour) {
-    return Math.round(secondsDifference / minute).toString() + '分前'
-  } else if (secondsDifference < day) {
-    return Math.round(secondsDifference / hour).toString() + '時間前'
-  } else if (secondsDifference < month) {
-    return Math.round(secondsDifference / day).toString() + '日前'
-  } else if (secondsDifference < year) {
-    return Math.round(secondsDifference / month).toString() + 'ヶ月前'
+  if (secondsDifference < MINUTE) {
+    return `${Math.floor(secondsDifference)}秒前`
+  } else if (secondsDifference < HOUR) {
+    return `${Math.floor(secondsDifference / MINUTE)}分前`
+  } else if (secondsDifference < DAY) {
+    return `${Math.floor(secondsDifference / HOUR)}時間前`
+  } else if (secondsDifference < MONTH) {
+    return `${Math.floor(secondsDifference / DAY)}日前`
+  } else if (secondsDifference < YEAR) {
+    return `${Math.floor(secondsDifference / MONTH)}ヶ月前`
+  } else {
+    return `${Math.floor(secondsDifference / YEAR)}年前`
   }
-  return Math.round(secondsDifference / year).toString() + '年前'
 }
