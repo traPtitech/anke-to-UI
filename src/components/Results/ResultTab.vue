5ac0d0c7309b5640867b62ec23095d0d77b23617
Fogrexon <fogrexon@gmail.com>
2021-02-25 18:40:29 +0900

    async 削除

--- src/components/Results/ResultTab.vue
+++ src/components/Results/ResultTab.vue
@@ -33,7 +33,7 @@ export default defineComponent({
 
     watch(
       () => route.query,
-      async newQuery => {
+      newQuery => {
         selectedTab.value = <string>newQuery.tab
       }
     )
