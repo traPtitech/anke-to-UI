84ad9ee1136c6dd64a0f841241a28e187efc6f1f
tantakaa <tantaka.azti@gmail.com>
2021-04-14 14:06:17 +0900

    fix css

--- src/pages/Responses.vue
+++ src/pages/Responses.vue
@@ -1,16 +1,18 @@
 <template>
-  <div :class="$style.title">自分の回答</div>
-  <div :class="$style.content">
-    <table :class="$style.table">
-      <th
-        v-for="(header, index) in headers"
-        :key="index"
-        :class="$style.header"
-      >
-        {{ header }}
-      </th>
-      <responses-table :response-summaries="responseSummaries" />
-    </table>
+  <div :class="$style.card">
+    <div :class="$style.title">自分の回答</div>
+    <div :class="$style.content">
+      <table :class="$style.table">
+        <th
+          v-for="(header, index) in headers"
+          :key="index"
+          :class="$style.header"
+        >
+          {{ header }}
+        </th>
+        <responses-table :response-summaries="responseSummaries" />
+      </table>
+    </div>
   </div>
 </template>
 
@@ -40,11 +42,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss" module>
+.card {
+  /* width: fit-content; */
+  margin: 1rem 1.5rem;
+  overflow-x: auto;
+  width: auto;
+  max-width: fit-content;
+  overflow-wrap: normal;
+}
 .title {
   font-size: 14pt;
   text-align: left;
   padding: 0.8em;
   border: solid 1.5px #dfe0d7;
+  letter-spacing: 0.05em;
 }
 .content {
   padding: 1em;
