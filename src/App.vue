705e52c23dd1773e14544c63ffe3a95d7592d13c
annin3 <shunta.titech@gmail.com>
2021-05-02 03:42:26 +0900

    途中

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div :class="$style.container">
     <header-component />
-    <side-bar :class="$style.sidebar" />
+    <side-bar v-if="!canSideBarShown" :class="$style.desktopSideBar" />
+    <div v-else v-show="isSideBarShown" :class="$style.mobileSideBarWrapper">
+      <side-bar :class="$style.mobileSideBar" />
+    </div>
     <main :class="$style.content">
       <router-view />
     </main>
@@ -9,15 +12,55 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, computed, onBeforeMount, watch, readonly } from 'vue'
 import HeaderComponent from '/@/components/Navigation/Header.vue'
 import SideBar from '/@/components/Navigation/SideBar.vue'
+import useOpener from '/@/use/opener'
+import useIsMobile from '/@/use/isMobile'
+import { useStore } from 'vuex'
+
+const useNavigationShown = () => {
+  const { isOpen, toggle: sideBarShown } = useOpener()
+  const { isMobile } = useIsMobile()
+
+  const isSideBarShown = computed(() => !isMobile.value || isOpen.value)
+  watch(isMobile, isMobile => {
+    isOpen.value = !isMobile
+  })
+
+  return {
+    isSideBarShown,
+    canSideBarshown: readonly(isMobile),
+    sideBarShown
+  }
+}
 
 export default defineComponent({
   name: 'App',
   components: {
     HeaderComponent,
     SideBar
+  },
+  setup() {
+    const {
+      isSideBarShown,
+      canSideBarshown,
+      sideBarShown
+    } = useNavigationShown()
+    const store = useStore()
+    const fetchedMe = computed(() => store.state.me !== null)
+
+    onBeforeMount(() => {
+      if (fetchedMe.value) return
+      store.dispatch.fetchMe()
+    })
+
+    return {
+      isSideBarShown,
+      canSideBarshown,
+      sideBarShown,
+      fetchedMe
+    }
   }
 })
 </script>
@@ -35,13 +78,25 @@ export default defineComponent({
   grid-template-columns: max(12rem, 15%) 1fr;
 }
 .content {
-  grid-area: content;
-  padding: 1.5rem;
-}
-.sidebar {
-  display: none;
-  @media (min-width: 1024px) {
-    display: block;
+  flex: 1;
+  overflow: {
+    x: hidden;
+    y: scroll;
   }
+}
+.desktopSideBar {
+  flex-shrink: 0;
+}
+.mobileSideBarWrapper {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+}
+.mobileSideBar {
+  width: 260px;
+  flex-shrink: 0;
 }
 </style>
