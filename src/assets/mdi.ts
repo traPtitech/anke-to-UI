49310bd1f89ee3785193b16ebc346ee312efc7cf
annin3 <shunta.titech@gmail.com>
2021-05-27 23:56:39 +0900

    fix conflict

--- src/assets/mdi.ts
+++ src/assets/mdi.ts
@@ -1,19 +1,32 @@
 import {
   mdiChevronDown,
+  mdiChevronUp,
   mdiMagnify,
   mdiOpenInNew,
   mdiHelpCircleOutline,
   mdiMenu,
-  mdiClose
+  mdiClose,
+  mdiEyeOff,
+  mdiEye,
+  mdiClipboard,
+  mdiDownload,
+  mdiArrowLeft
 } from '@mdi/js'
 
 const mdi = new Map<string, string>([
   ['chevron-down', mdiChevronDown],
+  ['chevron-up', mdiChevronUp],
   ['magnify', mdiMagnify],
   ['open-in-new', mdiOpenInNew],
   ['help-circle-outline', mdiHelpCircleOutline],
   ['menu', mdiMenu],
-  ['close', mdiClose]
+  ['close', mdiClose],
+  ['chevron-down', mdiChevronDown],
+  ['eye-closed', mdiEyeOff],
+  ['eye', mdiEye],
+  ['clipboard', mdiClipboard],
+  ['download', mdiDownload],
+  ['arrow-left', mdiArrowLeft]
 ])
 
 export default mdi
