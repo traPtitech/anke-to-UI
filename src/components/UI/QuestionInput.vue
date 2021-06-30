bc93c891abb1bb05839f3dfbb55d6d70328178ef
kohbanye <kohbanye@gmail.com>
2021-06-30 09:53:06 +0900

    add question input component

---
+++ src/components/UI/QuestionInput.vue
@@ -0,0 +1,57 @@
+<template>
+  <input
+    type="text"
+    :class="$style.input"
+    :value="modelValue"
+    placeholder="回答"
+    @input="update"
+  />
+  <div :class="$style.underline"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'QuestionInput',
+  props: {
+    modelValue: {
+      type: String,
+      required: true
+    }
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    'update:modelValue': (value: string) => true
+  },
+  setup(props, context) {
+    const update = (e: InputEvent) => {
+      context.emit('update:modelValue', (e.target as HTMLInputElement).value)
+    }
+
+    return { update }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.input {
+  box-sizing: border-box;
+  width: 100%;
+  border: none;
+  outline: 0;
+  padding: 0.4rem;
+  border-bottom: 1px dotted #7c6c4d;
+  &:focus + .underline {
+    transform: scaleX(1);
+  }
+}
+.underline {
+  width: 100%;
+  height: 2px;
+  margin-top: -1px;
+  background-color: #7c6c4d;
+  transform: scaleX(0);
+  transition: transform 0.1s;
+}
+</style>
