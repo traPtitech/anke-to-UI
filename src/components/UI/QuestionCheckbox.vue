6fafbad35d9c7d8f745998d6d70b27d2cd223af9
kohbanye <kohbanye@gmail.com>
2021-07-09 12:19:32 +0900

    add checkbox component

---
+++ src/components/UI/QuestionCheckbox.vue
@@ -0,0 +1,106 @@
+<template>
+  <div :class="$style.container">
+    <label
+      v-for="(content, index) in contents"
+      :key="index"
+      :class="$style.label"
+    >
+      <span :class="$style.check">
+        <icon
+          name="check"
+          :class="[checked[index] ? $style.checked : '', $style.icon]"
+        />
+      </span>
+      <input type="checkbox" :class="$style.checkbox" @input="check(index)" />
+      <p :class="$style.content">{{ content }}</p>
+    </label>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, ref } from 'vue'
+import Icon from '/@/components/UI/Icon.vue'
+
+export default defineComponent({
+  name: 'QuestionCheckbox',
+  components: {
+    Icon
+  },
+  props: {
+    contents: {
+      type: Array as PropType<string[]>,
+      required: true
+    },
+    modelValue: {
+      type: Array as PropType<string[]>,
+      required: true
+    }
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    'update:modelValue': (v: string[]) => true
+  },
+  setup(props, context) {
+    const checked = ref(new Array<boolean>(props.contents.length).fill(false))
+    const checkedContents: string[] = []
+
+    const check = (index: number) => {
+      checked.value[index] = !checked.value[index]
+
+      const content = props.contents[index]
+      if (checkedContents.includes(content)) {
+        checkedContents.splice(index, 1)
+      } else {
+        checkedContents.push(content)
+      }
+
+      context.emit('update:modelValue', checkedContents)
+    }
+
+    return { check, checked }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0.5rem;
+  max-width: 100%;
+  .label {
+    display: flex;
+    flex-direction: row;
+    margin: 0.2rem 0;
+    .content {
+      margin: 0;
+      margin-left: 0.4rem;
+    }
+  }
+}
+.check {
+  display: inline-block;
+  cursor: pointer;
+  content: '';
+  height: 1rem;
+  width: 1rem;
+  border: 1px solid #7c6c4d;
+  border-radius: 0.2rem;
+  &:hover {
+    opacity: 0.6;
+  }
+  .icon {
+    opacity: 0;
+    height: 100%;
+    width: 100%;
+    transition: 0.2s;
+  }
+  .checked {
+    opacity: 1;
+  }
+}
+.checkbox {
+  appearance: none;
+}
+</style>
