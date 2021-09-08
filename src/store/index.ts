7fe3a360e96d523d91d1a9bb61786b2ebd69d54e
annin3 <shunta.titech@gmail.com>
2021-09-08 10:02:58 +0900

    delete count

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,20 +2,10 @@ import { createDirectStore } from 'direct-vuex'
 import apis, { Me } from '/@/lib/apis'
 const { store, rootActionContext } = createDirectStore({
   state: {
-    count: 0,
     me: null as Me | null
   },
-  getters: {
-    countString(state) {
-      if (state.count === 0) return 'zero'
-      if (state.count === 1) return 'once'
-      return `${state.count} times`
-    }
-  },
+  getters: {},
   mutations: {
-    increment(state) {
-      state.count++
-    },
     setMe(state, me: Me) {
       state.me = me
     }
