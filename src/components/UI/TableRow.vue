2070659035e34b675243d94f6bf23b53ee492cad
tantakaa <tantaka.azti@gmail.com>
2021-09-14 22:00:26 +0900

    Create TableRow.vue

---
+++ src/components/UI/TableRow.vue
@@ -0,0 +1,29 @@
+<template>
+  <tr :class="$style.container">
+    <slot />
+  </tr>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TableRow',
+  components: {},
+  props: {},
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  text-align: left;
+  font-size: 1rem;
+  border-bottom: solid 1.5px #d9d9d9;
+  &:nth-child(even) {
+    background-color: #fafafa;
+  }
+}
+</style>
