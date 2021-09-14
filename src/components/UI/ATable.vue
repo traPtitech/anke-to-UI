35f93207b15cf81a4129078dc60762c6f4170ce1
tantakaa <tantaka.azti@gmail.com>
2021-09-14 21:59:26 +0900

    ATable.vueを変更し、\components\UI\~ に移した

---
+++ src/components/UI/ATable.vue
@@ -0,0 +1,28 @@
+<template>
+  <table :class="$style.tablecontainer">
+    <tr :class="$style.header">
+      <slot name="tableheader"></slot>
+    </tr>
+    <slot name="tablecontent"></slot>
+  </table>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'ATable'
+})
+</script>
+
+<style lang="scss" module>
+.tablecontainer {
+  max-width: fit-content;
+  margin: 1rem;
+  font-size: 1rem;
+  border-collapse: collapse;
+}
+.header {
+  width: 100%;
+  border-bottom: solid 2px #dbdbdb;
+}
+</style>
