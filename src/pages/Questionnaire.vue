d0834f64e4f252e08f4be9f6f7bfb34e308f153f
wakashi1 <makotozuoteng78@gmail.com>
2021-09-30 21:59:17 +0900

    セーブ２

--- src/pages/Questionnaire.vue
+++ src/pages/Questionnaire.vue
@@ -7,18 +7,25 @@
 
 <script lang="ts">
 import { defineComponent, ref, onMounted } from 'vue'
-import apis, { QuestionnaireByID } from '../lib/apis'
+import { useRoute } from 'vue-router'
+import apis, { QuestionnaireByID, ResponseSummary } from '../lib/apis'
 import Informations from '/@/components/Questionnaire/Informations.vue'
 
 export default defineComponent({
   name: 'Questionnaire',
   components: { Informations },
   setup() {
+    const route = useRoute()
     const questionnaire = ref<QuestionnaireByID | null>(null)
+    const myresponses = ref<ResponseSummary[]>([])
 
     onMounted(async () => {
-      const { data } = await apis.getQuestionnaire(1)
+      const questionnaireId = Number(route.params.id)
+      if (isNaN(questionnaireId)) return
+      const { data } = await apis.getQuestionnaire(questionnaireId, '')
+      const deta = await apis.getMyResponses(questionnaireId, '')
       questionnaire.value = data
+      myresponses.value = deta.data
     })
 
     return { questionnaire }
