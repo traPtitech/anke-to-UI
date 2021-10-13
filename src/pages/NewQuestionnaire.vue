d3234026c2f658f517bef667e9e52e5961457063
koh <kohbanye@gmail.com>
2021-10-13 10:10:22 +0900

    add tab

--- src/pages/NewQuestionnaire.vue
+++ src/pages/NewQuestionnaire.vue
@@ -1,20 +1,33 @@
 <template>
-  <Information />
+  <Tab v-model="selectedTab" :tabs="tabs" :class="$style.tab" />
+  <Information v-if="selectedTab === 'Information'" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import Information from '/@/components/NewQuestionnaire/Information.vue'
+import Tab from '/@/components/UI/Tab.vue'
 import { useTitle } from './use/title'
 
 export default defineComponent({
   name: 'NewQuestionnaire',
   components: {
-    Information
+    Information,
+    Tab
   },
   setup() {
     useTitle(ref('新規アンケート作成'))
-    return {}
+
+    const tabs = ['Information', 'Questions']
+    const selectedTab = ref(tabs[0])
+
+    return { tabs, selectedTab }
   }
 })
 </script>
+
+<style lang="scss" module>
+.tab {
+  margin-bottom: 1rem;
+}
+</style>
