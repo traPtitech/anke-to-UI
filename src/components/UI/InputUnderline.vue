e173f360b5d49dccf2ae29f3de3515d21fb9c864
wakashi1 <makotozuoteng78@gmail.com>
2022-05-29 11:49:36 +0900

    fix color in UI/InputUnderline.vue

--- src/components/UI/InputUnderline.vue
+++ src/components/UI/InputUnderline.vue
@@ -1,18 +1,11 @@
 <template>
-  <div :class="[$style.underline, isBlue ? $style.blue : '']" />
+  <div :class="$style.underline" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-
 export default defineComponent({
   name: 'InputUnderline',
-  props: {
-    isBlue: {
-      type: Boolean,
-      required: true
-    }
-  },
   setup() {
     return {}
   }
@@ -20,20 +13,16 @@ export default defineComponent({
 </script>
 
 <style lang="scss" module>
-$red: #ac4848;
-$blue: #005bac;
+$accent-primary: #ac4848;
 .underline {
   width: 100%;
   height: 2px;
-  background-color: $red;
+  background-color: $accent-primary;
   transform: scaleX(0);
   transition: transform 0.1s;
   textarea:focus + &,
   input:focus + & {
     transform: scaleX(1);
   }
-}
-.blue {
-  background-color: $blue;
 }
 </style>
