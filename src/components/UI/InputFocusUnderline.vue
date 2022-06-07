ca2df3dff6e42bebd2cc87a1bc5b44c3db3d78fc
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-06-07 14:35:24 +0900

    create InputFocusUnderline.vue

---
+++ src/components/UI/InputFocusUnderline.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :class="$style.focusunderline" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'InputFocusUnderline',
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.focusunderline {
+  width: 100%;
+  height: 1px;
+  background-color: $accent-primary;
+  transform: scaleX(0);
+  transition: transform 0.1s;
+  textarea:focus + &,
+  input:focus + & {
+    transform: scaleX(1);
+  }
+}
+</style>
