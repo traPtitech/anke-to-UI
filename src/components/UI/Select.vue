b8e103e8a9b7407933846c82cc6d2f431394f94e
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-06-18 15:50:40 +0900

    fix css in UI/Select.vue

--- src/components/UI/Select.vue
+++ src/components/UI/Select.vue
@@ -1,9 +1,19 @@
 <template>
-  <select :value="modelValue" @change="update($event.target.value)">
-    <option v-for="(content, index) in contents" :key="index" :value="content">
-      {{ content }}
-    </option>
-  </select>
+  <div :class="$style.select">
+    <select
+      :class="$style.display"
+      :value="modelValue"
+      @change="update($event.target.value)"
+    >
+      <option
+        v-for="(content, index) in contents"
+        :key="index"
+        :value="content"
+      >
+        {{ content }}
+      </option>
+    </select>
+  </div>
 </template>
 
 <script lang="ts">
@@ -37,3 +47,36 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" module>
+.display {
+  display: flex;
+  align-items: center;
+  position: relative;
+  appearance: none;
+  width: 200px;
+  height: 40px;
+  border-radius: 6px;
+  background-color: $ui-white;
+  border-color: $ui-secondary;
+  padding: 4px 50px 4px 8px;
+}
+.select {
+  position: relative;
+  width: 200px;
+  height: 40px;
+}
+.select::after {
+  content: '';
+  position: absolute;
+  right: 12px;
+  top: 50%;
+  width: 10px;
+  height: 10px;
+  border-top: 3px solid $ui-primary;
+  border-left: 3px solid $ui-primary;
+  transform: translateY(-50%) rotate(-135deg);
+  font-size: 20px;
+  pointer-events: none;
+}
+</style>
