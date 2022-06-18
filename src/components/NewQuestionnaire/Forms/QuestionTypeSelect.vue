ec50c7c6281ebd7d1393c52b4eaa738071be9df4
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-06-18 16:00:55 +0900

    create QuestionTypeSelect.vue

---
+++ src/components/NewQuestionnaire/Forms/QuestionTypeSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <Select
+    :contents="questionType"
+    :model-value="modelValuetype"
+    @update:model-value="update"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue'
+import Select from '/@/components/UI/Select.vue'
+import { questionTypes, findQuestionTypes } from '../use/utils'
+import { QuestionType } from '/@/lib/apis'
+
+export default defineComponent({
+  name: 'QuestionTypeSelect',
+  components: {
+    Select
+  },
+  props: {
+    modelValue: {
+      type: String as PropType<QuestionType>,
+      required: true
+    }
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    'update:type': (type: QuestionType) => true
+  },
+  setup(props, context) {
+    const questionType = [
+      'テキスト',
+      'テキスト（長文）',
+      '数値',
+      'チェックボックス',
+      'ラジオボタン',
+      '目盛り'
+    ]
+    const modelValuetype = computed(() => questionTypes[props.modelValue].label)
+    const update = (type: string) => {
+      context.emit('update:type', findQuestionTypes(type))
+    }
+    return { modelValuetype, questionType, update }
+  }
+})
+</script>
