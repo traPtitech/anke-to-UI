da2c209cba11905155ca2b9c03dade6e5754ee87
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-06-29 15:23:59 +0900

    fix TextForm.vue

--- src/components/NewQuestionnaire/Forms/TextForm.vue
+++ src/components/NewQuestionnaire/Forms/TextForm.vue
@@ -4,40 +4,19 @@
     :is-required="questionData.is_required"
     @update:name="updateQuestionName"
     @update:required="updateQuestionRequired"
-  >
-  </QuestionForm>
-  <InputText
-    v-if="questionData.question_type === 'Number'"
-    :is-number="true"
-    :placeholder="'0'"
-    :non-events="true"
-  />
-  <InputText
-    v-else-if="questionData.question_type === 'Text'"
-    :placeholder="'回答を入力'"
-    :non-events="true"
-  />
-  <InputText
-    v-else
-    :is-number="false"
-    :is-long="true"
-    :placeholder="'回答を入力'"
-    :non-events="true"
   />
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
 import QuestionForm from './QuestionForm.vue'
-import InputText from '/@/components/UI/InputText.vue'
 import { TextQuestion } from '../use/utils'
 import { updateQuestionData } from '../use/updateQuestionData'
 
 export default defineComponent({
   name: 'TextForm',
   components: {
-    QuestionForm,
-    InputText
+    QuestionForm
   },
   props: {
     questionData: {
