c10ec61299263c7e1e358acaa58eb3960a21fd2f
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-07-05 14:33:00 +0900

    fix ChoiceElement.vue

--- src/components/NewQuestionnaire/Forms/ChoiceElement.vue
+++ src/components/NewQuestionnaire/Forms/ChoiceElement.vue
@@ -1,29 +1,34 @@
 <template>
-  <input :type="[isRadio ? 'radio' : 'checkbox']" value="true" />
-  <InputText
-    v-model="labelRef"
-    :model-value="label"
-    :placeholder="'選択肢を入力'"
-  />
-  <Icon name="delete" @click="deleteChoice" />
+  <div :class="$style.choice">
+    <RadioButton v-if="isRadio" :disabled="true" />
+    <Checkbox v-else :disabled="true" />
+    <InputTextNoBorder
+      v-model="labelRef"
+      :model-value="label"
+      :placeholder="'選択肢を入力'"
+      :class="$style.input"
+      :is-focus="isFocus"
+    />
+    <Icon name="close" :class="$style.close" @click="deleteChoice" />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, watch } from 'vue'
 import Icon from '../../UI/Icon.vue'
-import InputText from '../../UI/InputText.vue'
+import Checkbox from '/@/components/UI/QuestionCheckboxIcon.vue'
+import RadioButton from '/@/components/UI/QuestionRadioButtonIcon.vue'
+import InputTextNoBorder from '/@/components/UI/InputTextNoBorder.vue'
 
 export default defineComponent({
   name: 'ChoiceElement',
   components: {
     Icon,
-    InputText
+    Checkbox,
+    RadioButton,
+    InputTextNoBorder
   },
   props: {
-    index: {
-      type: Number,
-      required: true
-    },
     label: {
       type: String,
       required: true
@@ -31,21 +36,25 @@ export default defineComponent({
     isRadio: {
       type: Boolean,
       default: false
+    },
+    isFocus: {
+      type: Boolean,
+      default: false
     }
   },
   emits: {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    'update:label': (text: string, index: number) => true,
+    'update:label': (text: string) => true,
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    delete: (index: number) => true
+    delete: () => true
   },
   setup(props, context) {
     const labelRef = ref(props.label)
     watch(labelRef, newLabel => {
-      context.emit('update:label', newLabel, props.index)
+      context.emit('update:label', newLabel)
     })
     const deleteChoice = () => {
-      context.emit('delete', props.index)
+      context.emit('delete')
     }
     return {
       labelRef,
@@ -54,3 +63,17 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" module>
+.choice {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+.input {
+  flex-grow: 1;
+}
+.close {
+  margin-left: 8px;
+}
+</style>
