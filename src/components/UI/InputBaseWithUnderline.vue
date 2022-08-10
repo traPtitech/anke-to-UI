e259b5f62d777bbd94e3fa9977f369fb2b03d622
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-08-10 16:33:26 +0900

    create InputBaseWithUnderline.vue

---
+++ src/components/UI/InputBaseWithUnderline.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <slot />
+    <input-focus-underline :class="$style.underline" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import InputFocusUnderline from './InputFocusUnderline.vue'
+
+export default defineComponent({
+  name: 'InputBaseWithUnderline',
+  components: {
+    InputFocusUnderline
+  }
+})
+</script>
+
+<style lang="scss" module>
+.underline {
+  position: relative;
+  z-index: 2;
+  margin-top: -1px;
+}
+</style>
