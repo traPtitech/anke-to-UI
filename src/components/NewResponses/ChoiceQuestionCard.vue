c9ce66ba1bfa8d2b67f5039930f7f7a9db4120dd
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-08-12 21:14:27 +0900

    create ChoiceQuestionCard.vue

---
+++ src/components/NewResponses/ChoiceQuestionCard.vue
@@ -0,0 +1,72 @@
+<template>
+  <question-card-base :title="title" :is-required="isRequired">
+    <div v-if="questionType === QuestionType.Checkbox">
+      <QuestionCheckbox
+        :contents="options"
+        :model-value="modelValue"
+        @update:model-value="update"
+      />
+    </div>
+    <div v-else>
+      <QuestionRadio
+        :contents="options"
+        :model-value="modelValue[0]"
+        @update:model-value="updateRadio"
+      />
+    </div>
+  </question-card-base>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { QuestionType } from '/@/lib/apis'
+import QuestionCardBase from '/@/components/UI/QuestionCardBase.vue'
+import QuestionCheckbox from '/@/components/UI/QuestionCheckbox.vue'
+import QuestionRadio from '/@/components/UI/QuestionRadio.vue'
+
+export default defineComponent({
+  name: 'InputQuestionCard',
+  components: {
+    QuestionCardBase,
+    QuestionCheckbox,
+    QuestionRadio
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    isRequired: {
+      type: Boolean,
+      default: false
+    },
+    questionType: {
+      type: String as PropType<QuestionType>,
+      required: true
+    },
+    options: {
+      type: Array as PropType<string[]>,
+      default: []
+    },
+    modelValue: {
+      type: Array as PropType<string[]>,
+      required: true
+    }
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    update: (choice: string[]) => true
+  },
+  setup(props, context) {
+    const update = (choice: string[]) => {
+      context.emit('update', choice)
+    }
+    const updateRadio = (choice: string) => {
+      context.emit('update', [choice])
+    }
+    return { QuestionType, update, updateRadio }
+  }
+})
+</script>
+
+<style lang="scss" module></style>
