08d43a8dc126a3754a3c0803f02df31a701e86c7
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-09-08 16:57:41 +0900

    fix

--- src/components/UI/LinearScale.vue
+++ src/components/UI/LinearScale.vue
@@ -4,7 +4,7 @@
     <label
       v-for="(num, index) in range"
       :key="index"
-      :class="[disabled ? $style.disabledlabel : '']"
+      :class="[disabled ? $style.disabledlabel : '', $style.label]"
     >
       <div>{{ num }}</div>
       <radio-button
@@ -70,6 +70,11 @@ export default defineComponent({
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+}
+.label {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 .leftLabel {
   margin-right: 0.5rem;
