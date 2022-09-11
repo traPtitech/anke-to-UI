3e531de341ed7e90a00983c8939bdf41483ce9ac
annin3 <shunta.titech@gmail.com>
2022-09-12 02:25:27 +0900

    アンケートサマリー、管理者・回答者等のレイアウト変更

--- src/components/Questionnaire/Information/Detail.vue
+++ src/components/Questionnaire/Information/Detail.vue
@@ -1,40 +1,29 @@
 <template>
-  <Card>
-    <template #header>情報</template>
-    <template #content>
-      <div>結果:{{ information.res_shared_to }}</div>
-      <div>
-        <details open>
-          <summary>管理者({{ information.administrators.length }})</summary>
-          {{ information.administrators.join(', ') }}
-        </details>
-      </div>
-      <div>
-        <details open>
-          <summary>回答済みの人({{ information.respondents.length }})</summary>
-          {{ information.respondents.join(', ') }}
-        </details>
-      </div>
-      <div>
-        <details open>
-          <summary>対象者({{ information.targets.length }})</summary>
-          {{ information.targets.join(', ') }}
-        </details>
-      </div>
-      <div>更新日時：{{ information.modified_at }}</div>
-      <div>作成日時：{{ information.created_at }}</div>
-    </template>
-  </Card>
+  <div :class="$style.container">
+    <DetailTextRow
+      :term="'管理者'"
+      :description="information.administrators.join(', ')"
+    />
+    <DetailTextRow
+      :term="'対象者'"
+      :description="information.targets.join(', ')"
+    />
+    <!-- ここに対照グループを追加 -->
+    <DetailTextRow
+      :term="'回答済み'"
+      :description="information.respondents.join(', ')"
+    />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, PropType } from 'vue'
-import Card from '/@/components/UI/Card.vue'
 import { QuestionnaireByID } from '/@/lib/apis'
+import DetailTextRow from './DetailTextRow.vue'
 
 export default defineComponent({
   name: 'Summary',
-  components: { Card },
+  components: { DetailTextRow },
   props: {
     information: {
       type: Object as PropType<QuestionnaireByID>,
@@ -46,3 +35,14 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss" module>
+.container {
+  background: $bg-secondary;
+  display: flex;
+  flex-direction: column;
+  padding: 0.75rem 1rem;
+  gap: 2rem;
+  border-radius: 0.5rem;
+}
+</style>
---
+++ src/components/Questionnaire/Information/DetailTextRow.vue
@@ -0,0 +1,57 @@
+<template>
+  <div :class="[$style.container, isMobile ? $style.mobile : '']">
+    <div :class="$style.term">
+      {{ term }}
+    </div>
+    <div :class="$style.description">
+      {{ description }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import useIsMobile from '/@/use/isMobile'
+
+export default defineComponent({
+  name: 'DetailTextRow',
+  props: {
+    term: {
+      type: String,
+      default: 'Text',
+      required: true
+    },
+    description: {
+      type: String,
+      default: 'Text',
+      required: true
+    }
+  },
+  setup() {
+    const { isMobile } = useIsMobile()
+    return { isMobile: isMobile }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.container {
+  display: flex;
+  gap: 2rem;
+  font-weight: 700;
+  font-size: 0.875rem;
+  text-align: left;
+}
+.mobile {
+  flex-direction: column;
+  gap: 0.5rem;
+}
+.term {
+  width: 5rem;
+  color: $ui-secondary;
+}
+.description {
+  width: 100%;
+  color: $ui-primary;
+}
+</style>
