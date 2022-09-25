a6f762f2c15293fd542aa05b5717cbd2389655fd
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-09-25 23:46:04 +0900

    create ViewCardBase.vue

---
+++ src/components/Results/Statistics/ViewCardBase.vue
@@ -0,0 +1,58 @@
+<template>
+  <Card>
+    <template #content>
+      <div :class="$style.head">
+        <div :class="$style.title">{{ title }}</div>
+        <div :class="$style.copybutton">
+          <icon :name="'content-copy'" @click="copy" />
+        </div>
+      </div>
+      <slot name="statistics"></slot>
+    </template>
+  </Card>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from '/@/components/UI/Card.vue'
+import Icon from '/@/components/UI/Icon.vue'
+
+export default defineComponent({
+  name: 'ResultCard',
+  components: {
+    Card,
+    Icon
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  emits: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    copy: () => true
+  },
+  setup(props, context) {
+    const copy = () => {
+      context.emit('copy')
+    }
+    return { copy }
+  }
+})
+</script>
+
+<style lang="scss" module>
+.head {
+  display: flex;
+  flex-direction: row;
+}
+.title {
+  @include size-body;
+  font-weight: 700;
+}
+.copybutton {
+  position: relative;
+  margin-left: auto;
+}
+</style>
