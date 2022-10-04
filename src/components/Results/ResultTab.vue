f965091e9e4c0242543c39322786f540e679792b
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-10-04 22:25:05 +0900

    fix ResultTab.vue

--- src/components/Results/ResultTab.vue
+++ src/components/Results/ResultTab.vue
@@ -2,20 +2,23 @@
   <PageTemplate>
     <template #header>
       <div :class="$style.container">
-        <div :class="$style.title">{{ questionnaire.title }}</div>
-        <div :class="$style.rightcontent">
+        <div :class="$style.title">
+          {{ resultsPerQuestion.questionnaire.title }}
+        </div>
+        <div v-if="tabType === '概要'" :class="$style.rightcontent">
           <icon :name="'download'" @click="isOpen = !isOpen" />
           <dropdown-contents
             :contents="downloadTypes"
             :is-open="isOpen"
             @close="isOpen = !isOpen"
-            @change-option="download"
+            @change-option="downloadType"
           />
         </div>
       </div>
       <div :class="$style.container">
         <Tab v-model="tabType" :tabs="tabTypes" />
         <dropdown-form
+          v-if="tabType === '概要'"
           v-model="formType"
           :contents="formTypes"
           :class="$style.rightcontent"
@@ -25,24 +28,16 @@
     <template #content>
       <Statistics
         v-if="tabType === '概要'"
-        :questionnaire="questionnaire"
-        :results="results"
-        :questions="questions"
         :form-type="formType"
+        :results-per-question="resultsPerQuestion"
       />
-      <Individual
-        v-if="tabType === '個別'"
-        :questionnaire="questionnaire"
-        :results="results"
-        :questions="questions"
-      />
+      <Individual v-if="tabType === '個別'" />
     </template>
   </PageTemplate>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, PropType } from 'vue'
-import { QuestionnaireByID, ResponseResult, QuestionDetails } from '/@/lib/apis'
 import Tab from '/@/components/UI/Tab.vue'
 import DropdownContents from '/@/components/Explorer/DropdownContents.vue'
 import Icon from '/@/components/UI/Icon.vue'
@@ -54,11 +49,15 @@ import {
   tabTypes,
   TabTypes,
   downloadTypes,
-  DownloadTypes,
   formTypes,
   FormTypes
 } from './use/utils'
-import { ResultsPerQuestion } from '/@/lib/util/statistics'
+import { ResultsPerQuestion } from './use/statistics'
+import {
+  generateQuestionMarkdownSpreadseet,
+  generateQuestionCSVSpreadseet,
+  download
+} from './use/DownloadForm'
 
 export default defineComponent({
   name: 'ResultTab',
@@ -72,34 +71,34 @@ export default defineComponent({
     Tab
   },
   props: {
-    questionnaire: {
-      type: Object as PropType<QuestionnaireByID>,
-      required: true
-    },
-    results: {
-      type: Object as PropType<ResponseResult[]>,
-      required: true
-    },
-    questions: {
-      type: Object as PropType<QuestionDetails[]>,
-      required: true
-    },
     resultsPerQuestion: {
       type: Object as PropType<ResultsPerQuestion>,
       required: true
     }
   },
-  setup() {
+  setup(props) {
     const tabType = ref<TabTypes>('概要')
     const isOpen = ref<boolean>(false)
-    const download = (value: string) => {
-      alert(value)
-    }
     const formType = ref<FormTypes>('Markdown')
+    const downloadType = (value: string) => {
+      if (value === 'Markdownでダウンロード') {
+        download(
+          props.resultsPerQuestion.questionnaire.title,
+          generateQuestionMarkdownSpreadseet(props.resultsPerQuestion),
+          'md'
+        )
+      } else {
+        download(
+          props.resultsPerQuestion.questionnaire.title,
+          generateQuestionCSVSpreadseet(props.resultsPerQuestion),
+          'csv'
+        )
+      }
+    }
     return {
+      downloadType,
       isOpen,
       downloadTypes,
-      download,
       formType,
       formTypes,
       tabType,
@@ -111,8 +110,8 @@ export default defineComponent({
 
 <style lang="scss" module>
 .title {
-  @include size-body;
-  font-weight: 700;
+  @include size-head;
+  @include font-bold;
   text-align: left;
 }
 .container {
