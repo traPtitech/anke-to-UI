fc50f7cf1ead5bdb250150c974e2910a4effa909
annin3 <shunta.titech@gmail.com>
2022-11-22 22:09:13 +0900

    バグ修正、DOM順

--- src/components/UI/Pagination.vue
+++ src/components/UI/Pagination.vue
@@ -9,39 +9,91 @@
     />
     <PaginationButton
       :number="1"
-      :is-current="1 == current"
+      :is-current="current === 1"
       @click="updatePage(1)"
     />
-    <div v-for="(page, key) in 5" :key="key">
-      <icon
-        v-if="
-          (page == 5 && totalPage - current > 3) || (page == 1 && current > 4)
-        "
-        name="dots-horizontal"
-        :class="$style.dots"
+    <div
+      v-if="current < 5 || totalPage <= 7"
+      :class="$style.pagination_wrapper"
+    >
+      <PaginationButton
+        v-if="totalPage > 2"
+        :number="2"
+        :is-current="current === 2"
+        @click="updatePage(2)"
       />
       <PaginationButton
-        v-else-if="4 < current && totalPage - current > 2"
-        :number="current + page - 3"
-        :is-current="current + page - 3 == current"
-        @click="updatePage(current + page - 3)"
+        v-if="totalPage > 3"
+        :number="3"
+        :is-current="current === 3"
+        @click="updatePage(3)"
       />
       <PaginationButton
-        v-else-if="current <= 4 && page + 1 < totalPage"
-        :number="page + 1"
-        :is-current="page + 1 == current"
-        @click="updatePage(page + 1)"
+        v-if="totalPage > 4"
+        :number="4"
+        :is-current="current === 4"
+        @click="updatePage(4)"
       />
       <PaginationButton
-        v-else-if="totalPage - current <= 2 && page + 1 < totalPage"
-        :number="totalPage + page - 6"
-        :is-current="totalPage + page - 6 == current"
-        @click="updatePage(totalPage + page - 6)"
+        v-if="totalPage > 5"
+        :number="5"
+        :is-current="current === 5"
+        @click="updatePage(5)"
+      />
+      <icon v-if="totalPage > 7" name="dots-horizontal" :class="$style.dots" />
+      <PaginationButton
+        v-else-if="totalPage > 6"
+        :number="6"
+        :is-current="current === 6"
+        @click="updatePage(6)"
       />
     </div>
+    <div v-else-if="current > totalPage - 3" :class="$style.pagination_wrapper">
+      <icon name="dots-horizontal" :class="$style.dots" />
+      <PaginationButton
+        :number="totalPage - 4"
+        :is-current="current === totalPage - 4"
+        @click="updatePage(totalPage - 4)"
+      />
+      <PaginationButton
+        :number="totalPage - 3"
+        :is-current="current === totalPage - 3"
+        @click="updatePage(totalPage - 3)"
+      />
+      <PaginationButton
+        :number="totalPage - 2"
+        :is-current="current === totalPage - 2"
+        @click="updatePage(totalPage - 2)"
+      />
+      <PaginationButton
+        :number="totalPage - 1"
+        :is-current="current === totalPage - 1"
+        @click="updatePage(totalPage - 1)"
+      />
+    </div>
+    <div v-else :class="$style.pagination_wrapper">
+      <icon name="dots-horizontal" :class="$style.dots" />
+      <PaginationButton
+        :number="current - 1"
+        :is-current="false"
+        @click="updatePage(current - 1)"
+      />
+      <PaginationButton
+        :number="current"
+        :is-current="true"
+        @click="updatePage(current)"
+      />
+      <PaginationButton
+        :number="current + 1"
+        :is-current="false"
+        @click="updatePage(current + 1)"
+      />
+      <icon name="dots-horizontal" :class="$style.dots" />
+    </div>
     <PaginationButton
+      v-if="totalPage >= 2"
       :number="totalPage"
-      :is-current="totalPage == current"
+      :is-current="current === totalPage"
       @click="updatePage(totalPage)"
     />
     <icon
