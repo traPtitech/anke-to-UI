995b2ec73c02cbc9daee0161386dc18058a1f107
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2022-12-18 22:42:10 +0900

    fix Administrates.vue

--- src/pages/Administrates.vue
+++ src/pages/Administrates.vue
@@ -3,7 +3,14 @@
     <CardWithHeader>
       <template #header>自分が管理者になっているアンケート</template>
       <template #content>
-        <CardContentDetail :questionnaires="questionnaires" />
+        <transition name="fadeAdministrates">
+          <div v-if="isFetched">
+            <CardContentDetail :questionnaires="questionnaires" />
+          </div>
+          <div v-else>
+            <CardContentDetailMock />
+          </div>
+        </transition>
       </template>
     </CardWithHeader>
   </div>
@@ -14,23 +21,27 @@ import { defineComponent, onMounted, ref } from 'vue'
 import { useTitle } from './use/title'
 import CardWithHeader from '/@/components/UI/CardWithHeader.vue'
 import CardContentDetail from '/@/components/UI/CardContentDetail.vue'
+import CardContentDetailMock from '/@/components/UI/CardContentDetailMock.vue'
 import apis, { QuestionnaireMyAdministrates } from '/@/lib/apis'
 
 export default defineComponent({
   name: 'Administrates',
   components: {
     CardWithHeader,
-    CardContentDetail
+    CardContentDetail,
+    CardContentDetailMock
   },
   setup() {
     useTitle(ref('回答対象のアンケート一覧'))
 
     const questionnaires = ref<QuestionnaireMyAdministrates[]>([])
+    const isFetched = ref(false)
     onMounted(async () => {
       const { data } = await apis.getMyQuestionnaire()
       questionnaires.value = data
+      isFetched.value = true
     })
-    return { questionnaires }
+    return { questionnaires, isFetched }
   }
 })
 </script>
@@ -38,5 +49,15 @@ export default defineComponent({
 <style lang="scss" module>
 .card {
   max-width: 1280px;
+}
+:global {
+  .fadeAdministrates-enter-active,
+  .fadeAdministrates-leave-active {
+    transition: opacity 0.2s;
+  }
+  .fadeAdministrates-enter-from,
+  .fadeAdministrates-leave-to {
+    opacity: 0;
+  }
 }
 </style>
