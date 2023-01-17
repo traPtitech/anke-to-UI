ed23c5c1641e39daf570c7bd2024cf82f4da9d1b
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2023-01-17 14:58:47 +0900

    add KeepAnswerButton in NewResponses.vue

--- src/pages/NewResponses.vue
+++ src/pages/NewResponses.vue
@@ -1,8 +1,19 @@
 <template>
   <template v-if="questionnaire">
-    <NewResponsesTab
-      :questionnaire="questionnaire"
-      :questioncontents="questioncontents"
+    <div :class="$style.container">
+      <NewResponsesTab
+        :class="$style.tab"
+        :questionnaire="questionnaire"
+        :questioncontents="questioncontents"
+        @update:responsebody="value => (responseBody = value)"
+      />
+    </div>
+    <KeepAnswerButton
+      :keeptxt="'保存'"
+      :answertxt="'回答'"
+      :class="$style.button"
+      @keep="keepNewResponses"
+      @answer="answerNewResponses"
     />
   </template>
   <template v-else> 読み込み中</template>
@@ -12,16 +23,23 @@
 import { defineComponent, ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useTitle } from '/@/pages/use/title'
-import apis, { QuestionDetails, QuestionnaireByID } from '/@/lib/apis'
+import apis, {
+  PostResponse,
+  QuestionDetails,
+  QuestionnaireByID,
+  ResponseBody
+} from '/@/lib/apis'
 import NewResponsesTab from '/@/components/NewResponses/NewResponsesTab.vue'
+import KeepAnswerButton from '/@/components/UI/KeepAnswerButton.vue'
 
 export default defineComponent({
   name: 'NewResponses',
-  components: { NewResponsesTab },
+  components: { NewResponsesTab, KeepAnswerButton },
   setup() {
     const route = useRoute()
     const questionnaire = ref<QuestionnaireByID | null>(null)
     const questioncontents = ref<QuestionDetails[]>([])
+    const responseBody = ref<ResponseBody[]>([])
 
     onMounted(async () => {
       const questionnaireId = Number(route.params.id)
@@ -36,12 +54,44 @@ export default defineComponent({
       useTitle(ref(`${qdata.data.title}`))
     })
 
+    const keepNewResponses = () => {
+      const responses = makeResponses(true)
+      apis.postResponse(responses)
+    }
+    const answerNewResponses = () => {
+      const responses = makeResponses(false)
+      apis.postResponse(responses)
+    }
+    const makeResponses = (tmp: boolean): PostResponse => {
+      const date = new Date()
+      return {
+        questionnaireID: Number(questionnaire.value?.questionnaireID),
+        body: responseBody.value,
+        temporarily: tmp,
+        submitted_at: date.toISOString()
+      }
+    }
+
     return {
       questionnaire,
-      questioncontents
+      questioncontents,
+      responseBody,
+      keepNewResponses,
+      answerNewResponses
     }
   }
 })
 </script>
 
-<style lang="scss" module></style>
+<style lang="scss" module>
+.container {
+  max-width: 720px;
+  margin-right: auto;
+  margin-left: auto;
+}
+.button {
+  position: fixed;
+  right: 5rem;
+  bottom: 3rem;
+}
+</style>
