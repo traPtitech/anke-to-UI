4634839145af97f5656356b9035b811544d20683
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2023-01-23 23:59:20 +0900

    update mobile in NewResponses.vue

--- src/pages/NewResponses.vue
+++ src/pages/NewResponses.vue
@@ -8,7 +8,17 @@
         @update:responsebody="value => (responseBody = value)"
       />
     </div>
+    <div v-if="isMobile" :class="$style.footer">
+      <KeepAnswerButton
+        :keeptxt="'保存'"
+        :answertxt="'回答'"
+        :class="$style.mobile_button"
+        @keep="keepNewResponses"
+        @answer="answerNewResponses"
+      />
+    </div>
     <KeepAnswerButton
+      v-else
       :keeptxt="'保存'"
       :answertxt="'回答'"
       :class="$style.button"
@@ -31,6 +41,7 @@ import apis, {
 } from '/@/lib/apis'
 import NewResponsesTab from '/@/components/NewResponses/NewResponsesTab.vue'
 import KeepAnswerButton from '/@/components/UI/KeepAnswerButton.vue'
+import useIsMobile from '/@/use/isMobile'
 
 export default defineComponent({
   name: 'NewResponses',
@@ -71,13 +82,15 @@ export default defineComponent({
         submitted_at: date.toISOString()
       }
     }
+    const { isMobile } = useIsMobile()
 
     return {
       questionnaire,
       questioncontents,
       responseBody,
       keepNewResponses,
-      answerNewResponses
+      answerNewResponses,
+      isMobile
     }
   }
 })
@@ -88,10 +101,23 @@ export default defineComponent({
   max-width: 720px;
   margin-right: auto;
   margin-left: auto;
+  padding-bottom: 3rem;
 }
 .button {
   position: fixed;
   right: 5rem;
   bottom: 3rem;
+}
+.mobile_button {
+  margin-top: auto;
+}
+.footer {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  background-color: $ui-white;
+  width: 100%;
+  box-shadow: 0px -20px 20px -20px rgba(0, 0, 0, 0.25);
+  height: 3rem;
 }
 </style>
