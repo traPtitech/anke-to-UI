af6eff17e3fd306f5c35629fff62b496e8b132ef
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2023-02-27 00:35:52 +0900

    create Manipulationbase.vue

---
+++ src/components/NewInformation/ManipulationBase.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div :class="$style.head">
+      <div :class="$style.title">{{ title }}</div>
+      <slot name="title" :class="$style.button"></slot>
+    </div>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ManipulationBase',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  },
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" module>
+.head {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 4px;
+}
+.title {
+  @include size-title;
+  color: $ui-primary;
+  text-align: left;
+  flex-grow: 1;
+}
+</style>
