7c01a74ba1db8a1506e669259ea29221aac56c12
wakashi1 <84297444+wakashi1@users.noreply.github.com>
2023-03-06 23:59:57 +0900

    create ClearQuestionnaire.vue

---
+++ src/components/NewInformation/ClearQuestionnaire.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div :class="$style.clear">アンケートを削除</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'ClearQuestionnaire',
+  setup() {
+    return
+  }
+})
+</script>
+
+<style lang="scss" module>
+.clear {
+  color: $accent-error-highlight;
+  cursor: pointer;
+  @include size-body-small-4;
+  margin-left: auto;
+  margin-right: auto;
+  width: 10rem;
+}
+</style>
