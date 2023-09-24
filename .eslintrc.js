b25e3625ce25859efb1a4c9b7d8b602406e04cff
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-24 16:36:28 +0900

    chore: `eslint-plugin-prettier`を削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,8 +15,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:vue/vue3-recommended',
-    'plugin:prettier/recommended',
-    'prettier'
+    'plugin:prettier/recommended'
   ],
   rules: {
     'no-console': 'warn',
